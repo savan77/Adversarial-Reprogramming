3a0f6ac7a9c52b1fc9d3b0fa156feeebab674204
Savan Visalpara <savan77@users.noreply.github.com>
2018-10-04 13:15:25 +0530

    added script to generate square images

--- gen.py
+++ gen.py
@@ -17,6 +17,7 @@ class ImageGenerator:
 
 	def save_output(self, img, label):
 		img.save(osp.join(self.out_dir, str(self.count)+".jpg"))
+		print("Saving {}".format(osp.join(self.out_dir, str(self.count)+".jpg")))
 		with open(osp.join(self.out_dir, str(self.count)+".txt"), "w") as f:
 			f.write(str(label))
 
@@ -29,41 +30,13 @@ class ImageGenerator:
 	def draw_squares(self, img):
 		label, where = self.get_prob()
 		draw = ImageDraw.Draw(img)
-		print(label, where)
+		place_map = {1:[0,0,9,9],2:[18,0,27,9],3:[9,9,18,18],4:[27,9,36,18],5:[0,18,9,27],\
+		6:[9,18,18,27],7:[27,0,36,9],8:[18,18,27,27],9:[18,27,27,36],10:[9,27,18,36],11:[9,0,18,9],\
+		12:[18,9,27,18],13:[27,27,36,36],14:[0,9,9,18],15:[0,27,9,36],16:[27,18,36,27]}
 		for i in range(label):
 			place = where[i]
-			if place == 1:
-				draw.rectangle([0,0,9,9], fill=(255,255,255), outline=(0,0,0))
-			elif place == 2:
-				draw.rectangle([9,0,18,9], fill=(255,255,255), outline=(0,0,0))
-			elif place == 3:
-				draw.rectangle([18,0,27,9], fill=(255,255,255), outline=(0,0,0))
-			elif place == 4:
-				draw.rectangle([27,0,36,9], fill=(255,255,255), outline=(0,0,0))
-			elif place == 5:
-				draw.rectangle([0,9,9,18], fill=(255,255,255), outline=(0,0,0))
-			elif place == 6:
-				draw.rectangle([9,9,18,18], fill=(255,255,255), outline=(0,0,0))
-			elif place == 7:
-				draw.rectangle([18,9,27,18], fill=(255,255,255), outline=(0,0,0))
-			elif place == 8:
-				draw.rectangle([27,9,36,18], fill=(255,255,255), outline=(0,0,0))
-			elif place == 9:
-				draw.rectangle([0,18,9,27], fill=(255,255,255), outline=(0,0,0))
-			elif place == 10:
-				draw.rectangle([9,18,18,27], fill=(255,255,255), outline=(0,0,0))
-			elif place == 11:
-				draw.rectangle([18,18,27,27], fill=(255,255,255), outline=(0,0,0))
-			elif place == 12:
-				draw.rectangle([27,18,36,27], fill=(255,255,255), outline=(0,0,0))
-			elif place == 13:
-				draw.rectangle([0,27,9,36], fill=(255,255,255), outline=(0,0,0))
-			elif place == 14:
-				draw.rectangle([9,27,18,36], fill=(255,255,255), outline=(0,0,0))
-			elif place == 15:
-				draw.rectangle([18,27,27,36], fill=(255,255,255), outline=(0,0,0))
-			elif place == 16:
-				draw.rectangle([27,27,36,36], fill=(255,255,255), outline=(0,0,0))
+			grid = place_map[place]
+			draw.rectangle(grid, fill=(255,255,255), outline=(0,0,0))
 
 		return img, label
 
